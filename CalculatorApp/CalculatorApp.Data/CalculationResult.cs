7ac3265b290d62119167a7feb35fa621c6708b4d
Maja <250348@edu.p.lodz.pl>
2025-03-23 22:00:44 +0100

    uwaga, chyba dziala

---
+++ CalculatorApp/CalculatorApp.Data/CalculationResult.cs
@@ -0,0 +1,18 @@
+﻿namespace CalculatorApp.Data
+{
+    public class CalculationResult
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+        public int Result {  get; set; }
+        public string Operation {  get; set; }
+
+        public CalculationResult (int firstNumber, int secondNumber, int result, string operation)
+        {
+            FirstNumber = firstNumber;
+            SecondNumber = secondNumber;
+            Result = result;
+            Operation = operation;
+        }
+    }
+}
---
+++ CalculatorApp/CalculatorApp.Logic/Calcuator.cs
@@ -0,0 +1,9 @@
+﻿using CalculatorApp.Data;
+
+namespace CalculatorApp.Logic
+{
+    public class Calcuator
+    {
+
+    }
+}
