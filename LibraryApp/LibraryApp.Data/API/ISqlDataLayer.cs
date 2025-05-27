38c3f4d117d6b79924a89e6663ff5e0d7a49e89e
lenka-schilling <250354@edu.p.lodz.pl>
2025-05-27 14:44:34 +0200

    separated sqldatalayer

---
+++ LibraryApp/LibraryApp.Data/API/ISqlDataLayer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp.Data.API
+{
+    public interface ISqlDataLayer
+    {
+        // --- USERS ---
+        void AddUser(int id, string name);
+        void RemoveUser(int id);
+        void UpdateUser(int id, string name);
+        IUser GetUser(int id);
+        IEnumerable<IUser> GetAllUsers();
+
+        // --- PRODUCTS ---
+        void AddProduct(int id, string name, int quantity);
+        void RemoveProduct(int id);
+        void UpdateProduct(int id, string name, int quantity);
+        IProduct GetProduct(int id);
+        IEnumerable<IProduct> GetAllProducts();
+
+        // --- EVENTS ---
+        void AddEvent(int id, string description, DateTime timestamp);
+        void RemoveEvent(int id);
+        void UpdateEvent(int id, string description, DateTime timestamp);
+        IEvent GetEvent(int id);
+        IEnumerable<IEvent> GetAllEvents();
+
+        // --- BORROW ---
+        void BorrowProduct(int productId);
+
+        // --- SAVE ---
+        void SaveChanges();
+    }
+}
--- LibraryApp/LibraryApp.Data/Implementation/SqlDataLayer.cs
+++ LibraryApp/LibraryApp.Data/Implementation/SqlDataLayer.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using LibraryApp.Data.API;
 using LibraryApp.Data.Implementation;
 
-namespace LibraryApp.Data
+namespace LibraryApp.Data.Implementation
 {
-    public class SqlDataLayer : IDataLayer
+    internal class SqlDataLayer : ISqlDataLayer
     {
         private readonly DataClasses1DataContext _context;
 
@@ -17,31 +17,27 @@ namespace LibraryApp.Data
 
         // --- USERS ---
 
-        public override void AddUser(int id, string name)
+        public void AddUser(int id, string name)
         {
-            var entity = new Users
-            {
-                Id = id,
-                Name = name
-            };
+            var entity = new Users { Id = id, Name = name };
             _context.Users.InsertOnSubmit(entity);
         }
 
-        public override void RemoveUser(int id)
+        public void RemoveUser(int id)
         {
             var user = _context.Users.FirstOrDefault(u => u.Id == id);
             if (user != null)
                 _context.Users.DeleteOnSubmit(user);
         }
 
-        public override void UpdateUser(int id, string name)
+        public void UpdateUser(int id, string name)
         {
             var user = _context.Users.FirstOrDefault(u => u.Id == id);
             if (user != null)
                 user.Name = name;
         }
 
-        public override IUser GetUser(int id)
+        public IUser GetUser(int id)
         {
             var user = _context.Users.FirstOrDefault(u => u.Id == id);
             if (user == null)
@@ -50,7 +46,7 @@ namespace LibraryApp.Data
             return new Reader(user.Id, user.Name);
         }
 
-        public override IEnumerable<IUser> GetAllUsers()
+        public IEnumerable<IUser> GetAllUsers()
         {
             return _context.Users
                 .Select(u => new Reader(u.Id, u.Name))
@@ -60,25 +56,20 @@ namespace LibraryApp.Data
 
         // --- PRODUCTS ---
 
-        public override void AddProduct(int id, string name, int quantity)
+        public void AddProduct(int id, string name, int quantity)
         {
-            var entity = new Products
-            {
-                Id = id,
-                Name = name,
-                Quantity = quantity
-            };
+            var entity = new Products { Id = id, Name = name, Quantity = quantity };
             _context.Products.InsertOnSubmit(entity);
         }
 
-        public override void RemoveProduct(int id)
+        public void RemoveProduct(int id)
         {
             var product = _context.Products.FirstOrDefault(p => p.Id == id);
             if (product != null)
                 _context.Products.DeleteOnSubmit(product);
         }
 
-        public override void UpdateProduct(int id, string name, int quantity)
+        public void UpdateProduct(int id, string name, int quantity)
         {
             var product = _context.Products.FirstOrDefault(p => p.Id == id);
             if (product != null)
@@ -88,19 +79,17 @@ namespace LibraryApp.Data
             }
         }
 
-        public override IProduct GetProduct(int id)
+        public IProduct GetProduct(int id)
         {
             var product = _context.Products.FirstOrDefault(p => p.Id == id);
             if (product == null)
                 return null;
 
-            // Bezpieczne rzutowanie z int? i obsługa nullable
             int quantity = product.Quantity ?? 0;
-
             return new Book(product.Id, product.Name, quantity);
         }
 
-        public override IEnumerable<IProduct> GetAllProducts()
+        public IEnumerable<IProduct> GetAllProducts()
         {
             return _context.Products
                 .Select(p => new Book(p.Id, p.Name, p.Quantity ?? 0))
@@ -110,25 +99,20 @@ namespace LibraryApp.Data
 
         // --- EVENTS ---
 
-        public override void AddEvent(int id, string description, DateTime timestamp)
+        public void AddEvent(int id, string description, DateTime timestamp)
         {
-            var entity = new Events
-            {
-                Id = id,
-                Description = description,
-                Timestamp = timestamp
-            };
+            var entity = new Events { Id = id, Description = description, Timestamp = timestamp };
             _context.Events.InsertOnSubmit(entity);
         }
 
-        public override void RemoveEvent(int id)
+        public void RemoveEvent(int id)
         {
             var ev = _context.Events.FirstOrDefault(e => e.Id == id);
             if (ev != null)
                 _context.Events.DeleteOnSubmit(ev);
         }
 
-        public override void UpdateEvent(int id, string description, DateTime timestamp)
+        public void UpdateEvent(int id, string description, DateTime timestamp)
         {
             var ev = _context.Events.FirstOrDefault(e => e.Id == id);
             if (ev != null)
@@ -138,19 +122,17 @@ namespace LibraryApp.Data
             }
         }
 
-        public override IEvent GetEvent(int id)
+        public IEvent GetEvent(int id)
         {
             var ev = _context.Events.FirstOrDefault(e => e.Id == id);
             if (ev == null)
                 return null;
 
-            // Bezpieczne rzutowanie DateTime? na DateTime
             DateTime timestamp = ev.Timestamp ?? DateTime.MinValue;
-
             return new BorrowEvent(ev.Id, ev.Description, timestamp);
         }
 
-        public override IEnumerable<IEvent> GetAllEvents()
+        public IEnumerable<IEvent> GetAllEvents()
         {
             return _context.Events
                 .Select(e => new BorrowEvent(e.Id, e.Description, e.Timestamp ?? DateTime.MinValue))
@@ -158,9 +140,9 @@ namespace LibraryApp.Data
                 .ToList();
         }
 
-        // --- BorrowProduct ---
+        // --- BORROW ---
 
-        public override void BorrowProduct(int productId)
+        public void BorrowProduct(int productId)
         {
             var product = _context.Products.FirstOrDefault(p => p.Id == productId);
             if (product == null)
@@ -173,9 +155,9 @@ namespace LibraryApp.Data
             product.Quantity = quantity - 1;
         }
 
-        // --- Save ---
+        // --- SAVE ---
 
-        public override void SaveChanges()
+        public void SaveChanges()
         {
             _context.SubmitChanges();
         }
